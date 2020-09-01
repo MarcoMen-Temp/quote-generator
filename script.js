c99b643a8630b230afad065c00b3245336b60530
MarcoMen-Temp <mrc_men@yahoo.ie>
2020-09-01 19:34:25 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// Show Loading
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+// Get Quote From API
+async function getQuote() {
+  showLoadingSpinner();
+  const proxyUrl = "https://secure-temple-77920.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    // console.log(data);
+    // If Author is blank, add 'Unknown'
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+
+    // Reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteContainer.classList.remove("long-quote");
+    }
+
+    quoteText.innerText = data.quoteText;
+
+    // Stop Loader and Show Quote
+    removeLoadingSpinner();
+    // throw new Error("oops");
+    // Add a counter for the errors
+    
+  } catch (error) {
+    console.log(error);
+    getQuote();
+    // console.log("Whoops, no quote", error);
+  }
+}
+
+// Tweet Quote
+
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuote();
